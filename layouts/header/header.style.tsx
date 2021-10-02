990b799af7eee849c8e0a8fc14c03bda4ad4d372
Javier <52850238+Jaserto@users.noreply.github.com>
2021-10-02 03:21:57 +0200

    mobile2

--- layouts/header/header.style.tsx
+++ layouts/header/header.style.tsx
@@ -7,22 +7,22 @@ export const HeaderWrapper = styled.header`
   width: 100%;
   justify-content:space-between;
   display: flex;
+  flex-direction:row;
   font-family: 'Inter', sans-serif;
   font-size: 18px;
   align-items: center;
   height: 130px;
   max-width:800px;
   color: white;
-  @media (max-width: 540px) {
+  .menu{
     display: flex;
-    flex-direction: column;
-    width: 100%;
+  flex-direction:row;
+    a{
+      margin-left: 15px;
+    }
   }
   @media (max-width: 830px) {
-    a{
-      display:flex;
-  
-    }
+   
     .menu{
      /*  border:1px solid red; */
       margin:0;
