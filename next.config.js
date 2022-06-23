aebc0e358fe6491032f1fa594393d32f3246866a
Jaserto <javiserna7@gmail.com>
2022-06-23 23:50:05 +0200

    actions 29

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-/* const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
@@ -9,29 +9,5 @@ module.exports = {
     path: '',
   },
   assetPrefix: isProd ? '/mdxblog/' : ''
-}*/
+}
 
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
-
-const withNextOptimizedImages = require('next-optimized-images');
-
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
-
-module.exports = withNextOptimizedImages(
-    withMDX({
-        webpack: (config, { isServer }) => {
-            if (!isServer) {
-            config.node = {
-            fs: 'empty'
-                }
-            }
-            return config
-        },
-      pageExtensions: ['js', 'jsx', 'mdx'],
-      target: 'serverless',
-        basePath: ghPages? '/mdxblog/' : '',
-        assetPrefix: ghPages ? '/mdxblog/' : '',
-    })
-)
